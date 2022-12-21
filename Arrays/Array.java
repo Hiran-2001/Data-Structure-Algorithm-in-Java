4c4f65b246f66a584cf4e7d81a65c2ef48f6c127
Hiran Raj <hirankvlr@gmail.com>
2022-12-21 14:10:46 +0530

    start learning DSA

---
+++ Arrays/Array.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Array
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        // int[] number = {5, 9 ,3 ,2};
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("enter the number to find out from array");
+        // int getNum = sc.nextInt();
+        // for (int i = 0; i < number.length; i++) {
+        //   if (number[i]==getNum) {
+        //     System.out.println("match found");
+        //   }else{
+        //     System.out.println("match not found");
+        //   }
+        //     // System.out.println(number[i]);
+        // }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name =  sc.nextLine();
+        System.out.println("Your name is" +  " " + name);
+    }
+}
