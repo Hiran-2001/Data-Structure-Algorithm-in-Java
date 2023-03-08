d0e7f5c573d604efe95a4dcd0e68e5ffe56b591a
Hiran Raj <hirankvlr@gmail.com>
2023-03-08 20:41:50 +0530

    working on arraylist

--- Arrays/ArrayListExample.java
+++ Arrays/ArrayListExample.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>(10);
+        ArrayList<Integer> list = new ArrayList<>(5);
 
-        list.add(5);
-        list.add(15);
-        list.add(25);
-        list.add(35);
-        list.add(45);
+      Scanner sc = new Scanner(System.in);
 
-        System.out.println(list);
+      for(int i = 0;i < 5;i++){
+
+          list.add(sc.nextInt());
+      }
+      for(int i = 0;i< list.size();i++){
+        System.out.println(list.get(i));
+        System.out.println("size of the array" + " " +list.size());
+      }
+
     }
+
 }
