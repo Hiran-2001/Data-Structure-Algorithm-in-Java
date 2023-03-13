611c50ce013077e3059822eea57b363723801afd
Hiran Raj <hirankvlr@gmail.com>
2023-03-13 16:05:36 +0530

    reworking on binary search

--- Searching/BinarySearch/OrderaAgnosticBS.java
+++ Searching/BinarySearch/OrderaAgnosticBS.java
@@ -1,46 +1,46 @@
 package Searching.BinarySearch;
 
+import java.util.Scanner;
+
 public class OrderaAgnosticBS {
     public static void main(String[] args) {
-        // int[] arr = {1,5,9,12,19,35,42,48,53,55,68};
-        int[] arr = { 68, 55, 53, 48, 42, 35, 19, 12, 9, 5, 1 };
-        int target = 68;
-        int result = Search(arr, target);
+        Scanner input = new Scanner(System.in);
+        int[] asdArr = { 1, 3, 8, 12, 19, 26, 29, 36, 46, 48, 50, 55 };
+        int[] desArr = { 55, 50, 48, 46, 36, 29, 26, 19, 12, 8, 3, 1 };
+        System.out.println("Enter a number to check whether it is in the array or not");
+        int target = input.nextInt();
+        int result = Search(desArr, target);
         System.out.println(result);
     }
 
     static int Search(int[] arr, int target) {
-
         int start = 0;
         int end = arr.length - 1;
-        // checking isAsending or isDesenging;
-        boolean isAsd = arr[start] < arr[end];
-         
 
-        // the common case for both asending and desending
+        boolean isAsd = arr[start] < arr[end];
+
         while (start <= end) {
             int mid = (start + end) / 2;
-            if (target == arr[mid]) {
+
+            if(target == arr[mid]){
                 return mid;
             }
 
             if (isAsd) {
-                if (target < arr[mid]) {
-                    end = mid - 1;
-                } else {
+                if (target > arr[mid]) {
                     start = mid + 1;
+                } else{
+                    end = mid - 1;
                 }
 
-            } else {
-                if (target > arr[mid]) {
-                    end = mid - 1;
-                } else {
+            }else{
+                if (target < arr[mid]) {
                     start = mid + 1;
+                } else{
+                    end = mid - 1;
                 }
             }
         }
-
         return -1;
-
     }
 }
