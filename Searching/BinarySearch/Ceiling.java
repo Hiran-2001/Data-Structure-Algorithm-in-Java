d1a412dc43677429b41ca98bda12fbf2239bd372
Hiran Raj <hirankvlr@gmail.com>
2023-03-16 22:34:14 +0530

    learned Ceiling ang Flooring

---
+++ Searching/BinarySearch/Ceiling.java
@@ -0,0 +1,33 @@
+package Searching.BinarySearch;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 7, 13, 15, 18, 21, 29, 40 };
+        int target = 30;
+        int result = Search(arr, target);
+        System.out.println(result);
+    }
+
+    static int Search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+        return start;
+    }
+}
---
+++ Searching/BinarySearch/Flooring.java
@@ -0,0 +1,33 @@
+package Searching.BinarySearch;
+
+public class Flooring {
+    public static void main(String[] args) {
+        int[] arr = { 1, 5, 16, 19, 26, 39, 46, 57, 60 };
+        int target = 17;
+        int result = SearchFlooring(arr, target);
+        System.out.println(result);
+    }
+
+    static int SearchFlooring(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+
+            } else {
+                return mid;
+            }
+        }
+        return end;
+    }
+}
