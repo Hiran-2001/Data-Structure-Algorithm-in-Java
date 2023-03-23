30ee6028513084a9e423662eacc688d94ad638a2
Hiran Raj <hirankvlr@gmail.com>
2023-03-23 10:19:05 +0530

    Working on binary search amazing questions

--- Searching/BinarySearch/FindPositionInfinite.java
+++ Searching/BinarySearch/FindPositionInfinite.java
@@ -1,39 +1,47 @@
 package Searching.BinarySearch;
 
-public class FindPostionInfinite {
+public class FindPositionInfinite {
+
     public static void main(String[] args) {
-        int[] arr = { 3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170 };
+
+        int[] arr = {3, 5, 7, 9, 10, 90,100, 130, 140, 160, 170,175,186};
         int target = 10;
-        int result = findIndex(arr, target);
-        System.out.println(result);
+
+        System.out.println(FindIndex(arr, target));
     }
 
-    static int findIndex(int[] arr, int target) {
-
+    static int FindIndex(int[] arr, int target){
+        
         int start = 0;
         int end = 1;
 
-        while (target > arr[end]) {
+        while(arr[end] < target){
             int newStart = end + 1;
-            end = end + (end - start + 1) * 2;
+
+            end = end + (end - start + 1)*2;
             start = newStart;
         }
-        return BinarySearch(arr, target, start, end);
+        // if(arr[end] >= target){
+        //     return end;
+        // }
+       return search(arr, target,start,end);
     }
 
-    static int BinarySearch(int[] arr, int target, int start, int end) {
+    static int search(int[] arr, int target,int start,int end) {
+        
+
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
             if (target == arr[mid]) {
                 return mid;
             }
+
             if (target < arr[mid]) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
-
         }
         return -1;
     }
