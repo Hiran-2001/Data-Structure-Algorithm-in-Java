7b78398c126207cdb95a21e674fad5e460d0ce45
Hiran Raj <hirankvlr@gmail.com>
2023-03-23 20:05:15 +0530

    solving binary search questions

---
+++ Searching/BinarySearch/PeakIndex.java
@@ -0,0 +1,25 @@
+package Searching.BinarySearch;
+
+public class PeakIndex {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 8, 5, 14, 3, 1 };
+        System.out.println(FindIndex(arr));
+    }
+
+    static int FindIndex(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                start = mid + 1;
+            }
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
