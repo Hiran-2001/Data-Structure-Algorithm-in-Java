3159bdf2c1e8c7c1e3a90d4d614c82945654fc89
Hiran Raj <hirankvlr@gmail.com>
2023-03-29 21:24:19 +0530

    some notes

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -13,10 +13,12 @@ public class Array {
 
         // arr[2] = 999;
    
-       Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in); // here we re using scanner class to get user input.
+
        System.out.println("enter five numbers");
-       for (int i = 0; i < arr.length; i++) {
-          arr[i] = sc.nextInt();
+
+       for (int i = 0; i < arr.length; i++) {  // Here the loop will work till the varible 'i' less than length of the array ie 5. 5 is the size of the array.
+          arr[i] = sc.nextInt();             //  here using scanner class we are taking user input and storing in that to array . arr[i] means arr[0]; 0 index; arr[1] first index
        }
         
     //    for(int j = 0 ; j< arr.length;j++){
